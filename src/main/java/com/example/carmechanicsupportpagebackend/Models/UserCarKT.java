f0cca01a28dbaeb428f6aef4a62e43d54d72c44c
Xhepton <dankajdx@gmail.com>
2023-10-18 16:23:57 +0200

    creating UserMapper and playing with database relationships

--- src/main/java/com/example/carmechanicsupportpagebackend/Models/UserCarKT.java
+++ src/main/java/com/example/carmechanicsupportpagebackend/Models/UserCarKT.java
@@ -7,13 +7,15 @@ import jakarta.persistence.*;
 public class UserCarKT {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "USER_ID", nullable = false)
-    private int user_id;
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @ManyToOne
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user_id;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "CAR_ID", nullable = false)
-    private int car_id;
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @OneToOne
+    @JoinColumn(name = "CAR_ID", nullable = false)
+    private Car car_id;
 
 }
---
+++ src/main/java/com/example/carmechanicsupportpagebackend/Services/Mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.example.carmechanicsupportpagebackend.Services.Mapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+}
