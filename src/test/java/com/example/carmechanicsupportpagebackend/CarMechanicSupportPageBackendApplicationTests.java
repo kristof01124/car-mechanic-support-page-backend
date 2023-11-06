b7a42fe739da7ac20566e9087817e45d0c5c9422
Sándor Martin Tibor <martinsandor707@gmail.com>
2023-11-06 23:09:48 +0100

    Add leftover unversioned files to git

---
+++ src/test/java/com/example/carmechanicsupportpagebackend/CarMechanicSupportPageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carmechanicsupportpagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMechanicSupportPageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
